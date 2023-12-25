d875efbfbe908b8434dce70c3a189c66701d04a4
jumgal23 <120317950+jumgal23@users.noreply.github.com>
2023-12-25 06:01:11 +0600

    занятие №59 , дабавил модель Проект

--- source/to_do_list/models.py
+++ source/to_do_list/models.py
@@ -17,6 +17,16 @@ class Status(models.Model):
         return self.name
 
 
+class Project(models.Model):
+    start_date = models.DateField(verbose_name='Дата начала')
+    end_date = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     description = models.CharField(max_length=255, verbose_name='Описание', validators=[MinLengthValidator(10)])
     detailed_description = models.TextField(blank=True, verbose_name='Детальное описание', validators=[MinLengthValidator(20)])
@@ -25,7 +35,11 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     types = models.ManyToManyField('to_do_list.Type', blank=True, related_name='articles', verbose_name='Типы')
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.SET_NULL, related_name='tasks',
+                                verbose_name='Проект')
 
     def __str__(self):
         return f'{self.id}. {self.description}. {self.status}'
+
+
 
